a83e472698a3f5131032af69ecdf1a3320dddb62
madhav23bansal <madhavb230100@gmail.com>
2020-12-11 13:29:33 +0530

    Added jwt and auth function

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 //Validations
@@ -49,8 +50,23 @@ router.post('/login',
         .isLength({ min: 6 })
         .withMessage("Password must be atleast 6 char long"),
     ],
-    (req,res)=>{
-        
+    async (req,res)=>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        //Checking if the email exists
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return res.status(400).send('Email doesn\'t exist');
+
+        //Checking if password is correct
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if(!validPass) return res.status(400).send('Invalid Password');
+
+        //Create and assign a jwt token 
+        const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+        res.header('auth-token',token).send(token);
     }
 );
 
